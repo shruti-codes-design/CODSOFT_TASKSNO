f672bae0fa10159c65d75ba954ab37d45b0e1eff
shruti-codes-design <basliyalshruti@gmail.com>
2026-07-10 22:07:09 +0530

    Added packet timestamp feature

--- Task-1_Network_Packet_Analyzer/main.py
+++ Task-1_Network_Packet_Analyzer/main.py
@@ -1,17 +1,52 @@
 from scapy.all import sniff
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, TCP, UDP
+from datetime import datetime
+
+# -----------------------------
+# Protocol Dictionary
+# -----------------------------
+PROTOCOLS = {
+    1: "ICMP",
+    6: "TCP",
+    17: "UDP"
+}
 
 
+# -----------------------------
+# Packet Processing Function
+# -----------------------------
 def packet_callback(packet):
+
     if packet.haslayer(IP):
-        print("=" * 50)
-        print(f"Source IP      : {packet[IP].src}")
-        print(f"Destination IP : {packet[IP].dst}")
-        print(f"Protocol       : {packet[IP].proto}")
-        print(f"Packet Length  : {len(packet)} bytes")
+
+        protocol = PROTOCOLS.get(packet[IP].proto, "Other")
+        current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+        print("=" * 60)
+        print("NETWORK PACKET")
+        print(f"Time             : {current_time}")
+
+        print(f"Source IP        : {packet[IP].src}")
+        print(f"Destination IP   : {packet[IP].dst}")
+        print(f"Protocol         : {protocol}")
+
+        if packet.haslayer(TCP):
+            print(f"Source Port      : {packet[TCP].sport}")
+            print(f"Destination Port : {packet[TCP].dport}")
+
+        elif packet.haslayer(UDP):
+            print(f"Source Port      : {packet[UDP].sport}")
+            print(f"Destination Port : {packet[UDP].dport}")
+
+        print(f"Packet Length    : {len(packet)} bytes")
 
 
-print("Network Packet Analyzer Started...")
+# -----------------------------
+# Main Program
+# -----------------------------
+print("\nNetwork Packet Analyzer Started...")
 print("Capturing 5 packets...\n")
 
 sniff(prn=packet_callback, count=5)
+
+print("\nCapture Completed.")
